9207293c6641aecc28279c2a817049ee24839618
M_Samei <42534934+ThunderBoy81@users.noreply.github.com>
2023-08-08 21:47:17 +0330

    Add files via upload

---
+++ A_turtle_hny/hny_image.py
@@ -0,0 +1,30 @@
+# In The Name Of God
+
+
+print('Please Wait...')
+import turtle
+
+skk = turtle.Turtle()
+
+skk.color('orange', 'yellow')
+skk.begin_fill()
+skk.pensize(5)
+skk.circle(100)
+skk.end_fill()
+
+skk.circle(100 , -80)
+skk.color('red')
+skk.write("  Happy New Year!", font=('Times New Roman',19))
+
+skk.color('orange', 'yellow')
+skk.circle(100 , 80)
+skk.right(90)
+skk.forward(5)
+skk.color('white')
+skk.forward(100)
+
+win = turtle.Screen()
+win.addshape('bunny-head.gif')
+skk.shape('bunny-head.gif')
+
+turtle.done()
